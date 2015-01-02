e258070090a3a91f35483662f55564585f44dd1c
Marcos Serpa <marcosserpa@gmail.com>
2015-01-02 00:38:32 -0200

    Adds rbenv settings Adds monologue gem; Sets monologue blogging configurations; Monkey patch of some - almost all - views; Configures routes; Adds an about section on right aside bar.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  mount Monologue::Engine, at: '/blog' # or whatever path, be it "/blog" or "/monologue"
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
